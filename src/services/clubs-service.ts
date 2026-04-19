376f2869d9c86158e47f567956130ea94143b5a7
Yury Rafael <badpiggiesyury@gmail.com>
2026-04-19 16:58:36 -0300

    Updating Clubs Service

--- src/services/clubs-service.ts
+++ src/services/clubs-service.ts
@@ -2,7 +2,15 @@ import * as httpResponse from "../utils/http-status-helper";
 import * as clubsRepository from '../repositories/club-repository';
 
 export const getClubService = async () => {
-    const data = await clubsRepository.findAllClubs();
-    const response = httpResponse.okStatus(data);
-    return response;
+    try {
+        const data = await clubsRepository.findAllClubs();
+
+        if (data.length === 0) {
+            return httpResponse.noContentStatus();
+        }
+
+        return httpResponse.okStatus(data);
+    } catch (error) {
+        return httpResponse.internalServerErrorStatus();
+    }
 }
